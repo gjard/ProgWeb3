c9ef04d15b841d039bfb7e3efff5178a21620c6a
gjard <90285873+gjard@users.noreply.github.com>
2021-11-30 21:27:25 -0400

    Etapa 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import API from './api'; 
 
 function App() {
     return <div>Comece aqui!</div>;
--- src/api.ts
+++ src/api.ts
@@ -2,3 +2,6 @@ import axios from 'axios';
 /**
  * Configurar o axios para a api: https://jsonplaceholder.typicode.com/
  */
+ export default axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com/'
+});
