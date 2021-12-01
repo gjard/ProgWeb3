f1466211b715d4423facc41c5f4e033d787780c2
gjard <90285873+gjard@users.noreply.github.com>
2021-11-30 21:48:37 -0400

    Etapa 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
 import API from './api'; 
 
-function App() {
-    return <div>Comece aqui!</div>;
+function App() { 
+ 
+    API.get('/posts')
+    .then(resposta => console.log(resposta))  
+    return (
+
+        <div>Comece aqui!</div>
+    );
 }
 
 export default App;
