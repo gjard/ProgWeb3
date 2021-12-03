9376a34ceb5e9bedaf1d5298913ac543d45d50fb
gjard <90285873+gjard@users.noreply.github.com>
2021-12-03 12:48:49 -0400

    Etapa 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,42 @@
-import { AxiosResponse } from 'axios';
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import API from './api'
 
-async function getRep(){
-    const resp = await API.get('/posts');
-    return resp.data
-    
-}
-
-//fetch('https://jsonplaceholder.typicode.com/posts', {
-//  method: 'POST',
-//  body: JSON.stringify({
-//    userId: 1,    CONDIÇÃO ONDE ESCOLHE QUAL USERID MOSTRAR
-//  }),
-//  headers: {
-//    'Content-type': 'application/json; charset=UTF-8',
-//  },
-//})
-//  .then((response) => response.json())
-//  .then((json) => console.log(json));
-
-
-
 function App() { 
-    const [posts, setPosts] = useState({});
-    console.log('POST')
-    console.log(posts)
-    
-    return (
-        <>
-        <div>Comece aqui!</div>
+    const [posts, setPosts] = useState([]);
+    function show(){API.get('/posts').then(add => setPosts(add.data));}
+    const arr = posts.map((e) => {return String(Object.values(e)[0]);})
+    var userId = arr.filter((j, i) => arr.indexOf(j) === i);
+    function UserId(){
+        let userIds: { label: string; value: string; }[]= []
 
-        <select id="selectOption">
-        <option value="A">UserId:1</option>
-        <option value="B">UserId:2</option>
-        <option value="C">UserId:3</option>
-        <option value="D">UserId:4</option>
-        <option value="E">UserId:5</option>
-        <option value="F">UserId:6</option>
-        <option value="G">UserId:7</option>
-        <option value="H">UserId:8</option>
-        <option value="I">UserId:9</option>
-        <option value="J">UserId:10</option>
-        </select>
-        
-        
-        <button onClick={() => setPosts({posts: getRep()})}>
-            POST
-        </button>
-        </>
-    );
-//FALTA A CONDIÇÃO DE QUE SE ESCOLHER UM VALOR IRA MOSTRAR APENAS UM USUÁRIO
+        userId.forEach(function(element) {
+            userIds.push({ label:element, value: element })
+        });
+        return userIds;
+    }
+    const [selectedOption, setSelectedOption] = useState(userId[0]);
+    return (
+    <>
+        <div>UserId:</div>
+        <select onClick={show} onChange={e => setSelectedOption(e.target.value)}>
+            <option>Selecione Aqui !</option>
+
+        {UserId().map(item => {return (<option key={item.label} value={item.value}>{item.value}</option>)})}      
+
+        </select> 
+        {posts.map((e) => {
+            if (String(Object.values(e)[0]) === selectedOption){
+                return(
+                    <><p>UserId:{String(Object.values(e)[0])}</p>
+                       <p>Id:{String(Object.values(e)[1])}</p>
+                       <p>Titulo:{String(Object.values(e)[2])}</p>
+                       <p>Corpo:{String(Object.values(e)[3])}</p>
+                    <br></br></>
+                );
+            }
+        })}
+ </>
+    ); 
 }
+//vamos la!!! falta pouco!!!
 export default App;
